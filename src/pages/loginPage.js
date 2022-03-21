28efe59a56ec835ab983236f9746cffec16926fd
Timothy Blom <kracok@gmail.com>
2022-03-21 13:49:14 +0100

    small updates

---
+++ src/pages/loginPage.js
@@ -0,0 +1,20 @@
+function LoginPage() {
+    return (
+      <div className="loginPage">
+
+        <div>
+            <img />
+            <div>
+                <p>Lost in translation</p>
+                <p>Get started</p>
+            </div>
+        </div>
+      
+        <input placeholder="What is your name?"></input>
+        <button>Go</button>
+
+      </div>
+    );
+  }
+  
+  export default LoginPage;
