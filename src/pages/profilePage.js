f25fe2374d90df69da477ec9359e34c110a8d0c1
Timothy Blom <kracok@gmail.com>
2022-03-22 20:02:07 +0100

    initial commit

--- src/pages/profilePage.js
+++ src/pages/profilePage.js
@@ -1,4 +1,5 @@
 import React, { Component }  from 'react';
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 
 function ProfilePage() {
     return (
@@ -6,17 +7,29 @@ function ProfilePage() {
         
         <div className='background'/>
 
-        <h1 className='lastSearched'>User name</h1>
-
         <div className='inputContainer'>
+            <h1 className='profileName'>User name</h1>
+        </div>
+
+        <div className='inputContainer bottom'>
             <div className='lastSearched'>
                 <h1>Last Searched Words</h1>
-                <p>#1</p>
+                <h2>#1 last searched word</h2>
+                <h2>#2 last searched word</h2>
+                <h2>#3 last searched word</h2>
+                <h2>#4 last searched word</h2>
+                <h2>#5 last searched word</h2>
+                <h2>#6 last searched word</h2>
+                <h2>#7 last searched word</h2>
+                <h2>#8 last searched word</h2>
+                <h2>#9 last searched word</h2>
+                <h2>#10 last searched word</h2>
             </div>
         </div>
 
-        <button className='logOutButton'>Log out</button>
-
+        <NavLink to='/'>
+            <button className='logOutButton'>Log out</button>
+        </NavLink>
       </div>
     );
   }
--- src/pages/translationPage.js
+++ src/pages/translationPage.js
@@ -1,9 +1,22 @@
-import React, { Component }  from 'react';
+import React, { Component, useState }  from 'react';
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import TranslatorLogo from '../translatorLogo.png'
 import GoButton from '../nextLogo.png'
 
 function TranslationPage() {
+  const [word, setWord] = useState([])
+
+  let translateWord;
+  let inputWord;
+
+  const onChange = e => {
+    inputWord = e.target.value;
+    translateWord = inputWord.split("")
+    console.log(inputWord)
+    console.log(translateWord)
+    // console.log(translateWord.replace('a', 'b'))
+  }
+
   return (
     <div className="TranslationPage">
 
@@ -11,11 +24,13 @@ function TranslationPage() {
 
       <div className='inputContainer'>
             <div className='input'>
-              <input className='inputField' placeholder="Type text you would like to translate"></input>
-              <NavLink to='/translate'>
-                <img className='inputButton' src={GoButton} />
-              </NavLink>
+              <input className='inputField' onChange={onChange} placeholder="Type text you would like to translate"></input>
+              <img className='inputButton' src={GoButton} onClick={() => setWord(translateWord)} />
             </div>
+        </div>
+
+        <div className='inputContainer bottom'>
+          {word}
         </div>
 
     </div>
