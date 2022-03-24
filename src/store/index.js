06a34978edc3803a65525ec79e9330efeba582cb
Timothy Blom <kracok@gmail.com>
2022-03-24 15:02:15 +0100

    updated store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from "redux-devtools-extension/logOnlyInProduction";
 import rootReducer from "./reducers";
 import rootMiddleware from "./middleware";
 
